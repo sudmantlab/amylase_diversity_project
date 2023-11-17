30532d3f91238466c0dc515733f9cd1e897cd3ea
Joana Rocha <42983167+joanocha@users.noreply.github.com>
2023-11-16 16:32:51 -0800

    Update and rename GWAS_AMY.py to hgdp_tgp_AMY.py

--- hdgp_tgp_AMY_popgen/hgdp_tgp_AMY.py
+++ hdgp_tgp_AMY_popgen/hgdp_tgp_AMY.py
@@ -9,9 +9,6 @@ import xarray as xr
 from sgkit.io.vcf import vcf_to_zarr
 from dask.diagnostics import ProgressBar
 import math
-pbar = ProgressBar()
-pbar.register()
-xr.set_options(display_expand_attrs=False, display_expand_data_vars=True);
 sample_table = pd.read_csv('CN_metadata_filtered.tsv', sep='\t') #has the correspondence between sample names and all other details
 superpopulations = sample_table['p2'].unique()
 superpopulations = ['AFR', 'AMR', 'CAS', 'EA', 'OCN', 'SA', 'WEA']
@@ -30,41 +27,21 @@ name_to_region = {v: k for k, v in region_mapping.items()}
 
 rule all:
     input: 
-        #'METADATA_4099_p1p2.tsv',
-        #'METADATA_4099_p1p2_no_trios.tsv',
-        #'METADATA_4099_p1p2_trio_samples_list.tsv',
-        #'CN_metadata.tsv',
-        #"CN_metadata_filtered.tsv",
-        #"CN_trio_samples_list.txt",
-        #expand('groups/{superpopulation}.txt', superpopulation=superpopulations),
-        #"groups/all_genotyped_individuals.txt",
-        #expand('hgdp.tgp.gwaspy.merged.{region}.merged.recode.vcf', region=region_mapping.values()), # recoded for regions with individuals with CN estimates not trios
-        #"hgdp.tgp.gwaspy.merged.b0_start_to_b0_end.merged.eigenvec",
-        #"hgdp.tgp.gwaspy.merged.b0_start_to_b0_end.merged.eigenval",
-        #"hgdp.tgp.gwaspy.merged.b1_start_to_b1a_end.merged.eigenvec",
-        #"hgdp.tgp.gwaspy.merged.b1_start_to_b1a_end.merged.eigenval",
-        #expand('hgdp.tgp.gwaspy.merged.{region}.merged.{superpopulation}.recode.vcf', region=region_mapping.values(), superpopulation=superpopulations), ## recoded for superpops  with CN estimates not trios
-        #expand('maf_filtered_hgdp.tgp.gwaspy.merged.b0_start_to_b1_end.merged.{superpopulation}.recode.vcf', superpopulation=superpopulations), ### filtered for minmaf 0.05  with CN estimates not trios
-        #expand("{superpopulation}_chr1.filtered.recode.norm.vcf", superpopulation=superpopulations),
-        #expand('chr1_{superpopulation}.ld.gz', superpopulation=superpopulations),
-        #expand("chr1_{superpopulation}.ld2.tsv.gz", superpopulation=superpopulations),
-        #expand("chr1_{superpopulation}.windowed.pi", superpopulation=superpopulations),
-        #expand("chr1_{superpopulation}.Tajima.D", superpopulation=superpopulations),
-        expand("chr1_{superpopulation}.ihs.tsv", superpopulation=superpopulations),
-        #"chr1_combined.ld2.tsv.gz",
-        #"chr1_combined.PI.tsv.gz",
-        #"chr1_combined.Tajima.D.tsv.gz",
+        "CN_metadata_filtered.tsv",
+        expand('groups/{superpopulation}.txt', superpopulation=superpopulations),
+        "groups/all_genotyped_individuals.txt",
+        expand('hgdp.tgp.gwaspy.merged.{region}.merged.recode.vcf', region=region_mapping.values()), # recoded for regions with individuals with CN estimates not trios
+        "hgdp.tgp.gwaspy.merged.b0_start_to_b0_end.merged.eigenvec",
+        "hgdp.tgp.gwaspy.merged.b0_start_to_b0_end.merged.eigenval",
+        "hgdp.tgp.gwaspy.merged.b1_start_to_b1a_end.merged.eigenvec",
+        "hgdp.tgp.gwaspy.merged.b1_start_to_b1a_end.merged.eigenval",
+        expand('hgdp.tgp.gwaspy.merged.{region}.merged.{superpopulation}.recode.vcf', region=region_mapping.values(), superpopulation=superpopulations), ## recoded for superpops  with CN estimates not trios
+        expand('maf_filtered_hgdp.tgp.gwaspy.merged.b0_start_to_b1_end.merged.{superpopulation}.recode.vcf', superpopulation=superpopulations), ### filtered for minmaf 0.05  with CN estimates not trios
+        "chr1_combined.ld2.tsv.gz",
+        "chr1_combined.PI.tsv.gz",
+        "chr1_combined.Tajima.D.tsv.gz",
         "chr1_combined.iHs.tsv",
-        #directory("hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode.zarr"),
-        #'gwas/hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode/AMY2A.tsv',  
-        #'gwas/hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode/AMY2B.tsv',
-        #'gwas/hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode/AMY1.tsv',
-        #'gwas/hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode/AMY2A.pdf',  
-        #'gwas/hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode/AMY2B.pdf',
-        #'gwas/hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode/AMY1.pdf',
-        #'gwas/hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode/AMY2A_qqplot.pdf',  
-        #'gwas/hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode/AMY2B_qqplot.pdf',
-        #'gwas/hgdp.tgp.gwaspy.merged.chr1.merged.EUR.recode/AMY1_qqplot.pdf',
+       
     
 #rule make_phenotype_table_from_raw:
 #    input: 'genotypes.raw.txt'
@@ -228,16 +205,6 @@ rule run_Pi:
     vcftools --vcf {input[0]}  --window-pi  20000 --out {params} 
     """
 
-rule run_Tajima: 
-    input:
-         "hgdp.tgp.gwaspy.merged.{region}.merged.{superpopulation}.recode.vcf"
-    output:
-        temp("{region}_{superpopulation}.Tajima.D"),
-    params: '{region}_{superpopulation}'
-    shell: """
-    vcftools --vcf {input[0]} --TajimaD 20000 --out {params} 
-    """
-
 rule calculate_ihs: #the script filters for maf 0.05
     input:
         hap_file = "{superpopulation}_chr1.filtered.recode.norm.vcf"
@@ -284,33 +251,6 @@ rule add_to_Pi:
             elif b1astart <= row['BIN_START'] <= b1aend and b1astart <= row['BIN_END'] <= b1aend:
                 return 'AMY region'
             elif amyst <= row['BIN_START'] <= amyend and amyst <= row['BIN_END'] <= amyend:
-                return 'amy genes'
-            else:
-                return 'chr1'
-        df['region'] = df.apply(assign_region, axis=1)
-        filtered_df_pi = df[df['region'] != 'amy genes']
-        filtered_df_pi.to_csv(output[0], sep='\t', compression='gzip', index=False)
-
-rule add_to_TajimaD:
-    input:
-        "{region}_{superpopulation}.Tajima.D"
-    output:
-        temp("{region}_{superpopulation}.Tajima.Dv2.gz")
-    params:
-        column_value="{superpopulation}"
-    run:
-        df = pd.read_table(input[0], sep='\t', usecols=["BIN_START", "N_SNPS", "TajimaD"])
-        df["Superpopulation"] = params.column_value
-        bstart, bend = 103456163, 103826698
-        b0start, b0end = 103456163, 103571526
-        amyst, amyend = 103571527, 103760697
-        b1astart, b1aend = 103760698, 103826698
-        def assign_region(row):
-            if b0start <= row['BIN_START'] <= b0end and b0start <= row['BIN_START'] <= b0end:
-                return 'AMY region'
-            elif b1astart <= row['BIN_START'] <= b1aend and b1astart <= row['BIN_START'] <= b1aend:
-                return 'AMY region'
-            elif amyst <= row['BIN_START'] <= amyend and amyst <= row['BIN_START'] <= amyend:
                 return 'amy genes'
             else:
                 return 'chr1'
@@ -377,23 +317,6 @@ rule concat_Pi_tables:
         done
         """
 
-rule concat_TajimaD_tables:
-    input:
-        expand("chr1_{superpopulation}.Tajima.Dv2.gz", superpopulation=superpopulations)
-    output:
-        "chr1_combined.Tajima.D.tsv.gz"
-    params:
-        files_to_concat=" ".join(expand("chr1_{superpopulation}.Tajima.Dv2.gz", superpopulation=superpopulations))
-    shell:
-        """
-        #!/bin/bash
-        zcat {input[0]} > {output}
-        for file in {params.files_to_concat}
-        do
-            zcat $file | tail -n +2 >> {output}
-        done
-        """
-
 rule concat_iHs_tables:
     input:
         expand("chr1_{superpopulation}.ihs.tsv", superpopulation=superpopulations)
@@ -410,96 +333,5 @@ rule concat_iHs_tables:
             cat $file | tail -n +2 >> {output}
         done
         """
-
-rule prepare_input_for_GWAS: 
-    input:
-        "{anything}.vcf"
-    output:
-        "{anything}.vcf.bgz",
-        "{anything}.vcf.bgz.tbi"
-    shell: """
-    bgzip -c {input[0]} > {output[0]} &&
-    tabix -p vcf {output[0]}
-    """
-
-rule vcf2zarr:
-    input:
-        "{anything}.vcf.bgz",
-    output:
-        directory("{anything}.zarr")
-    run:
-        vcf_to_zarr(input[0], output[0],  max_alt_alleles=1)
-
-
-rule run_gwas:
-    input: 
-        "{anything}.zarr",
-        'CN_metadata.tsv'
-    output: 
-        'gwas/{anything}/{gene}_corrected.tsv',
-        'gwas/{anything}/{gene}_corrected.pdf',
-        'gwas/{anything}/{gene}_qqplot_corrected.pdf'
-    run:
-        ds = sg.load_dataset(input[0])
-        cn = pd.read_csv(input[1], sep = '\t', index_col=0)
-        ds["variant_contig_name"] = (["variants"], [ds.contigs[contig] for contig in ds["variant_contig"].values])
-        print(ds["samples"])
-        ds2 = ds.set_index({"variants": ("variant_contig_name", "variant_position", "variant_id")})
-        ds_cn = pd.DataFrame.to_xarray(cn).rename({"ID":"samples"})
-        print(ds_cn)
-        ds = ds.set_index({"samples": "sample_id"})
-        ds = ds.merge(ds_cn, join="inner")
-        ds = ds.reset_index("samples").reset_coords(drop=True)
-        print(ds)
-        print(ds_cn.p2.to_series().value_counts())
-        ds["call_dosage"] = ds.call_genotype.sum(dim="ploidy")
-        #ds_lr = sg.gwas_linear_regression(ds, dosage="call_dosage", add_intercept=True, covariates=[], traits=[wildcards.gene]) 
-        ds_lr = sg.gwas_linear_regression(ds, dosage="call_dosage", add_intercept=True, covariates=["sex_imputation.is_female", "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10", "PC11", "PC12", "PC13", "PC14", "PC15", "PC16", "PC17", "PC18", "PC19", "PC20"], traits=[wildcards.gene])
-        print(ds_lr)
-        df = ds_lr[["variant_contig_name", "variant_contig", "variant_position", "variant_linreg_p_value", "variant_linreg_beta"]].to_dataframe()
-        print(df)
-        df.to_csv(output[0], sep='\t', index=False)
-        
-        #### Manhattan #####
-        df["variant_linreg_log_p_value"] = -np.log10(df["variant_linreg_p_value"])
-        df = df.astype({"variant_position": np.int64}) # to avoid overflow in cumulative_pos
-        running_pos = 0
-        cumulative_pos = []
-        for chrom, group_df in df.groupby("variant_contig"):
-            cumulative_pos.append(group_df["variant_position"] + running_pos)
-            running_pos += group_df["variant_position"].max()
-        df["cumulative_pos"] = pd.concat(cumulative_pos)
-        df["color group"] = df["variant_contig"].apply(lambda x : "A" if x % 2 == 0 else "B")
-        g = sns.relplot(
-            data = df,
-            x = "cumulative_pos",
-            y = "variant_linreg_log_p_value",
-            hue = "color group",
-            palette = ["darkgreen", "blue"],
-            linewidth=0,
-            s=10,
-            legend=None,
-            aspect=3
-        )
-        g.ax.set_xlabel("Chromosome 1")
-        #g.ax.set_xticks(df.groupby("variant_contig")["cumulative_pos"].median())
-        #g.ax.set_xticklabels(df["variant_contig_name"].unique())
-        plt.savefig(output[1], dpi=300)
-        
-        ### qqplot
-        p = ds_lr["variant_linreg_p_value"].squeeze().values
-        p.sort()
-        n = len(p)
-        expected_p = -np.log10(np.arange(1, n + 1) / n)
-        observed_p = -np.log10(p)
-        max_val = math.ceil(max(np.max(expected_p), np.max(observed_p)))
-        df = pd.DataFrame({"Expected -log10(p)": expected_p, "Observed -log10(p)": observed_p})
-        fig, ax = plt.subplots(figsize=(12, 12));
-        g = sns.scatterplot(data=df, x="Expected -log10(p)", y="Observed -log10(p)", ax=ax, linewidth=0)
-        x_pred = np.linspace(0, max_val, 50)
-        sns.lineplot(x=x_pred, y=x_pred, ax=ax)
-        g.set(xlim=(0, max_val), ylim=(0, max_val))
-        plt.savefig(output[2], dpi=300)
-
 
 
