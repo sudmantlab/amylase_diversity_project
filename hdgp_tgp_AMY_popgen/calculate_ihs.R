ea57a842e879de5b0632fe2d13dc1ea0e637a110
Joana Rocha <42983167+joanocha@users.noreply.github.com>
2023-11-17 07:10:16 -0800

    Update calculate_ihs.R

--- hdgp_tgp_AMY_popgen/calculate_ihs.R
+++ hdgp_tgp_AMY_popgen/calculate_ihs.R
@@ -1,11 +1,11 @@
 library(rehh)
 library(dplyr)
 
-# Get input parameters from Snakemake
+
 hap_file <- snakemake@input[["hap_file"]]
 output_file <- snakemake@output[["output_file"]]
 
-# Constants
+
 b0_start <- 103456163
 b0_end <- 103571526
 b1a_start <- 103760698
@@ -13,16 +13,15 @@ b1a_end <- 103826698
 b1b_start <- 103833698
 b1b_end <- 103863980
 
-# Process data
+
 data <- data2haplohh(hap_file = hap_file, polarize_vcf = FALSE)
 data_filt <- subset(data, min_maf = 0.05)
 data_scan <- scan_hh(data_filt, polarized = FALSE)
 data_ihs <- ihh2ihs(data_scan, freqbin = 1)
 
-# Extract the superpopulation from the file name
 superpopulation <- gsub("_chr1.filtered.recode.norm.vcf", "", basename(hap_file))
 
-# Add region and superpopulation columns
+
 data_ihs <- data_ihs$ihs %>%
   mutate(region = case_when(
     POSITION >= b0_start & POSITION <= b0_end ~ "AMY",
@@ -35,9 +34,9 @@ data_ihs <- data_ihs$ihs %>%
   superpop = superpopulation
   )
 
-# Filter data
+
 data_ihs_filtered <- data_ihs %>%
   filter(region != "AMY genes" & region != "recombination hotspot" & region != "b1b")
 
-# Write output
+
 write.table(data_ihs_filtered, output_file, sep="\t", row.names=FALSE, col.names=TRUE, quote=FALSE)
