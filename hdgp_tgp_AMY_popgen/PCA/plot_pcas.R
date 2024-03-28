77f5be82199b4081218e0eb8711c5d44a817ff0e
Joana Rocha <42983167+joanocha@users.noreply.github.com>
2024-03-28 10:06:53 -0700

    Update plot_pcas.R
    updated to plot PCAs by Superpopulation (facet in supp figures, non-facet in Extended data)

--- hdgp_tgp_AMY_popgen/PCA/plot_pcas.R
+++ hdgp_tgp_AMY_popgen/PCA/plot_pcas.R
@@ -107,3 +107,48 @@ print(test)
 
 ggsave("FigS6_sup_pop_pca_b0b1a.pdf", test, width=10, height=10, dpi=300) 
 ggsave("FigS6_sup_pop_pca_b0b1a.png", test, width=10, height=10, dpi=800) 
+
+#p2_nonfacet
+plot_pca_POPULATIONS<-function(eigenvector, eigenvalue, metadata, mytitle){
+  pca <- read_table(eigenvector, col_names = FALSE)
+  eigenval <- scan(eigenvalue,)
+  pca <- pca[,-1]
+  names(pca)[1] <- "ID"
+  names(pca)[2:ncol(pca)] <- paste0("PC", 1:(ncol(pca)-1))
+  dataset<-read.delim2(metadata, sep="\t", header=TRUE)
+  pca <- merge(dataset,pca,by="ID")
+  pve <- data.frame(PC = 1:20, pve = eigenval/sum(eigenval)*100)
+  var_explained <- ggplot(pve, aes(PC, pve)) + geom_bar(stat = "identity")+ ylab("Percentage variance explained") + theme_light()
+  cumsum(pve$pve)
+  data_to_hide <- filter(pca,  sample == 'HPRC')
+  colors1 <- c("#EDB829", "#DD4124", "#00496F","#0C7996","purple","#E97C07","#94B669")
+  pop_pca_bundle <- pca %>% 
+    filter(sample != 'HPRC') %>% 
+    ggplot(aes(PC1, PC2, col=p2)) +  # Keep the coloring by 'p2'
+    geom_point(size = 1) +
+    scale_color_manual(values = colors1, name = "Superpopulation") +  # Use your predefined colors
+    #facet_wrap(~p2, scales = "fixed", ncol = 1) +  # One column for facets
+    #facet_grid(p2 ~ ., scales = "fixed", switch = "x") +  # Facets in a single column with labels on the side
+    scale_shape_manual(values=c(0:25)) +  # Specify shape values
+    xlab(paste0("PC1 (", signif(pve$pve[1], 3), "%)")) + 
+    ylab(paste0("PC2 (", signif(pve$pve[2], 3), "%)")) + 
+    theme_cowplot() + 
+    ggtitle(mytitle) +
+    theme_cowplot() +
+    theme(legend.position = "bottom",  
+          strip.placement = "outside",  # Place the strips outside of the axes
+          #strip.background = element_blank(),
+          strip.background = element_rect(colour = "black", fill = NA, size = 0.5), 
+          panel.spacing = unit(0.1, "lines"),  # Reduce the space between facets
+          panel.border = element_rect(colour = "black", fill=NA, size=0.5)  # Add a border around each facet
+          
+    )
+  pop_pca_bundle
+}
+b0<-plot_pca_POPULATIONS("combined_bundle0.eigenvec", "combined_bundle0.eigenval", "CN_metadata_combined.tsv", "AMY left flank (bundle 0)")
+b1a<-plot_pca_POPULATIONS("combined_bundle1a.eigenvec", "combined_bundle1a.eigenval", "CN_metadata_combined.tsv","AMY right flank (bundle 1a)")
+
+ED2C<-ggarrange(b0, b1a, labels = c("A", "B"), ncol = 2, nrow = 1, common.legend = TRUE, legend = "bottom")
+print(ED2C)
+
+ggsave("FigED2_C_sup_pop_pca_b0b1a.pdf", ED2C, width=7, height=3, dpi=300) 
